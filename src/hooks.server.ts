f02219e4bf767b637bb07e7d18c23308deaabcb0
Gabriel Karczewski <nickelghost@outlook.com>
2024-04-19 22:52:33 +0200

    implement csp hashing properly

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
+import { createHash } from 'crypto';
 import { minify } from 'html-minifier-terser';
 
-const minification_options = {
+const minificationOptions = {
 	collapseWhitespace: true,
 	collapseInlineTagWhitespace: true,
 	removeComments: true,
@@ -14,7 +15,16 @@ const minification_options = {
 
 export const handle: Handle = async function ({ event, resolve }) {
 	const response = await resolve(event, {
-		transformPageChunk: ({ html }) => minify(html, minification_options)
+		transformPageChunk: async ({ html }) => {
+			const minified = await minify(html, minificationOptions);
+			const match = minified.match(/<script>(.*)<\/script>/);
+
+			if (!match) return minified;
+
+			const content = match[1];
+			const hash = createHash('sha256').update(content).digest('base64');
+			return minified.replace(/'sha256-[^\s]*';/, `'sha256-${hash}';`);
+		}
 	});
 
 	return response;
--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,16 @@ const config = {
 		adapter: adapter(),
 		env: {
 			publicPrefix: ''
+		},
+		csp: {
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', 'https://apis.google.com'],
+				'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+				'font-src': ['self', 'https://fonts.gstatic.com'],
+				'connect-src': ['self', 'https://*.googleapis.com', 'https://api.pantry.nickelghost.com'],
+				'frame-src': ['self', 'https://pantry.nickelghost.com']
+			}
 		}
 	}
 };
