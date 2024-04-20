5b98336ffe9a0ef1dc2f6ba3268cf49adffa2d2e
Gabriel Karczewski <nickelghost@outlook.com>
2024-04-20 16:05:56 +0200

    switch to redirect auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import LoadingIndicator from '$lib/components/LoadingIndicator.svelte';
 	import firebaseApp from '$lib/firebase/firebaseApp';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-	import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+	import {
+		GoogleAuthProvider,
+		getAuth,
+		onAuthStateChanged,
+		signInWithEmailAndPassword,
+		signInWithRedirect
+	} from 'firebase/auth';
 	import 'normalize.css';
 	import { onMount } from 'svelte';
 	import Modal from 'svelte-simple-modal';
@@ -12,21 +21,38 @@
 	const auth = getAuth(firebaseApp);
 
 	let shouldRender = false;
+	let isLoading = true;
 
 	onMount(async () => {
 		onAuthStateChanged(auth, async (user) => {
-			if (user) {
-				shouldRender = true;
-			} else {
-				const wasRendered = shouldRender;
-				shouldRender = false;
-				if (!wasRendered) {
-					await signInWithPopup(auth, provider);
-				}
-			}
+			isLoading = false;
+			shouldRender = !!user;
 		});
 	});
+
+	const handleEmailSignIn = async (e: SubmitEvent) => {
+		e.preventDefault();
+		isLoading = true;
+		const form = e.target as HTMLFormElement;
+		await signInWithEmailAndPassword(auth, form.email.value, form.password.value);
+	};
 </script>
+
+{#if isLoading}
+	<LoadingIndicator />
+{/if}
+
+{#if !shouldRender && !isLoading}
+	{#if window.location.hostname === 'localhost'}
+		<form on:submit={handleEmailSignIn}>
+			<Input type="email" label="Email" name="email" />
+			<Input type="password" label="Password" name="password" />
+			<Button type="submit">Sign in</Button>
+		</form>
+	{:else}
+		<Button on:click={async () => await signInWithRedirect(auth, provider)}>Google Sign in</Button>
+	{/if}
+{/if}
 
 {#if shouldRender}
 	<QueryClientProvider client={queryClient}>
@@ -48,6 +74,10 @@
 		display: grid;
 		font-family: 'Tauri', sans-serif;
 		grid-template-columns: 1fr minmax(0, 960px) 1fr;
+	}
+
+	body > * > * {
+		grid-column-start: 2;
 	}
 
 	h1,
